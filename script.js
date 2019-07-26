301943b42d4cfac427a12a4615cb4d7b77d511ce
s.ban@i.ua <s.ban@i.ua>
2019-07-26 12:56:15 +0300

    первый комит

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log(leftBorderWidth);
+
+var leftBorderWidth =1;
+let second =2;
+const pi =3.32;
+
+console.log(4/0);
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+console.log (incr);
+console.log (decr);
+
