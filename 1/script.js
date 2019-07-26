1b706126a1c9b587bded9cfd2f5e2947e8ccea86
s.ban@i.ua <s.ban@i.ua>
2019-07-26 17:00:26 +0300

    5 комит

--- 1/script.js
+++ 1/script.js
@@ -1,2 +1,12 @@
 // 'use strict';
 
+var num1 =12;
+var num2 = 8;
+console.log (num1 + num2);
+
+
+
+
+
+
+
---
+++ tast/script.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+var money = ("Ваш бюджет на месяц?");
+var time = ("Введите дату в формате YYYY-MM-DD");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    
+}
